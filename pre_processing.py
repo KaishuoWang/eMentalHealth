6908e524edefa114c274619f779793c069fa70f2
KaishuoWang <w15034653566@gmail.com>
2023-05-18 19:58:16 -0400

    infoSheet

--- pre_processing.py
+++ pre_processing.py
@@ -1,48 +1,22 @@
 # -*- coding: utf-8 -*-
 import re
+import pandas as pd
 
-class PreProcessing:
-    def __init__(self, records, taxonomy):
-        self.records = records
-        self.taxonomy = taxonomy
+def remove_HTML(raw):
+    CLEANER = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+    cleanText = re.sub(CLEANER, '', raw)
+    return cleanText.strip()
 
-    def cleanHtml(self, raw_html):
-        CLEANER = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
-        cleanText = re.sub(CLEANER, '', raw_html)
-        return cleanText
+def remove_new_line(raw):
+    cleanText = re.sub(r"[\r\n\t]+", ' ', raw)
+    return cleanText.strip()
 
-    def remove_new_line(self, raw):
-        cleanText = re.sub(r"[\r\n\t]+", '', raw)
-        return cleanText
-    
-    def preprocess(self):
-        print('Length of records before preprocessing:', len(self.records))
-        print('Length of taxonomy before preprocessing:', len(self.taxonomy))
-
-        self.records['description'] = self.records['description'].apply(lambda x: self.cleanHtml(x))
-        self.taxonomy['description'] = self.taxonomy['description'].apply(lambda x: self.cleanHtml(x))
-
-        self.records['description'] = self.records['description'].apply(lambda x: self.remove_new_line(x))
-        self.taxonomy['description'] = self.taxonomy['description'].apply(lambda x: self.remove_new_line(x))
-
-        # Remove empty description
-        indices = []
-        for index, row in self.records.iterrows():
-            if row['description'] == '':
-                indices.append(index)
-
-        self.records = self.records.drop(self.records.index[indices])
-        self.records.reset_index(drop=True, inplace=True)
-
-        indices = []
-        for index, row in self.taxonomy.iterrows():
-            if row['description'] == '':
-                indices.append(index)
-
-        self.taxonomy = self.taxonomy.drop(self.taxonomy.index[indices])
-        self.taxonomy.reset_index(drop=True, inplace=True)
-
-        print('Length of records after preprocessing:', len(self.records))
-        print('Length of taxonomy after preprocessing:', len(self.taxonomy))
-
-        return self.records, self.taxonomy
+# Remove empty description
+def remove_empty(column_name: str, df: pd.DataFrame) -> pd.DataFrame:
+    indices = []
+    for index, row in df.iterrows():
+        if row[column_name] == '':
+            indices.append(index)
+    df = df.drop(df.index[indices])
+    df.reset_index(drop=True, inplace=True)
+    return df
